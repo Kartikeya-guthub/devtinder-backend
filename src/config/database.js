cecfde00996b44efb08420c6e94bb4b276d61e77
Kartikeya <kartikeya9917@gmail.com>
2025-12-23 23:48:47 +0530

    Remove unused file

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async() =>{
-        await mongoose.connect("mongodb+srv://kartik20051234_db_user:eqe2CruJF9XI8vX2@cluster0.4cq7hz3.mongodb.net/devTinder")
+        await mongoose.connect("mongodb+srv://kartik20hnhnegnr051234_db_user:eqe2CruJrhrhryjF9XrgI8vX2@cluster0.4cq7hz3.mongodb.net/devTinder")
 }
 
 module.exports = connectDB;
